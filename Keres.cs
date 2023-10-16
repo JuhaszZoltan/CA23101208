580e6ac36a258ad5ccd4f73f74acdbe2ba64ce7b
Juhász Zoltán <juhaszzoltanmate@yahoo.com>
2023-10-16 09:52:08 +0200

    Add project files.

---
+++ Keres.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA23101208
+{
+    internal class Keres
+    {
+        public string Cim { get; set; }
+        public string DatumIdo { get; set; }
+        public string Get { get; set; }
+        public string AllapotKod { get; set; }
+        public string Meret { get; set; }
+
+        public Keres(string beolvasottSor)
+        {
+            string[] darabok = beolvasottSor.Split('*');
+            Cim = darabok[0];
+            DatumIdo = darabok[1];
+            Get = darabok[2];
+            string[] allapotEsMeret = darabok[3].Split(' ');
+            AllapotKod = allapotEsMeret[0];
+            Meret = allapotEsMeret[1];
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA23101208
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Keres> keresek = new List<Keres>();
+            using (StreamReader olvaso = new StreamReader(@"..\..\src\NASAlog.txt"))
+            {
+                while (!olvaso.EndOfStream)
+                {
+                    keresek.Add(new Keres(olvaso.ReadLine()));
+                }
+            }
+
+            Console.WriteLine($"5. feladat: Kérések száma: {keresek.Count}");
+
+            Console.ReadKey();
+        }
+    }
+}
